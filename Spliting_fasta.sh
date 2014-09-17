727e5bb2e182dc09136264c7d3c42cd95932fc43
Niklas Handin <handin_n@hotmail.com>
2014-09-17 20:02:34 +0200

    Script to split a fast file in to smaller parts and sbatch them with the information need to preform InterProScan

---
+++ Spliting_fasta.sh
@@ -0,0 +1,22 @@
+#Script to split a fasta file in to smaller sekvenses and creatig a bash file 
+#with the correct information. After that it is sent with sbatch to be executed
+FILE=$1
+mkdir "temporary_split"
+mkdir "temporary_script"
+	awk 'BEGIN {n_seq=0;} /^>/ {if(n_seq%3==0){file=sprintf("temporary_split/myseq%d.fa",n_seq);} print >> file; n_seq++; next;} { print >> file; }' $FILE
+FILES="temporary_split/*"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  name=$(basename "$f")
+  name="${name%.*}"
+  echo "#!/bin/bash -l" >> temporary_script/$name
+  echo "#SBATCH -A b2011098" >> temporary_script/$name
+  echo "#SBATCH -p core" >> temporary_script/$name
+  echo "#SBATCH -n 1" >> temporary_script/$name
+  echo "#SBATCH -t 0:00:10" >> temporary_script/$name
+  echo "#SBATCH -J $name" >> temporary_script/$name
+  echo "../interproscan.sh -t n -dp -appl Coils, Gene3D, PfamA, PRINTS, ProDom, SMART, SUPERFAMILY, TIGRFAM -i ../$f -b output/$name" >> temporary_script/$name
+  sbatch temporary_script/$name
+  echo "Done"
+done
